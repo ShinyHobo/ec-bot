2bcf369eb7f2d4dd345c64c03c4a52b7fba1bc57
ShinyHobo <shinyhobo1@gmail.com>
2021-10-02 13:02:28 -0500

    main: Moved to command structure

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ db.close();
 // Set up bot
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const botCommands = require('./commands');
+
+Object.keys(botCommands).map(key => {
+  bot.commands.set(botCommands[key].name, botCommands[key]);
+});
+
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
@@ -22,16 +29,16 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('pong');
-    msg.channel.send('pong pong');
+  const args = msg.content.split(/ +/);
+  const command = args.shift().toLowerCase();
+  console.info(`Called command: ${command}`);
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+  if (!bot.commands.has(command)) return;
+
+  try {
+    bot.commands.get(command).execute(msg, args);
+  } catch (error) {
+    console.error(error);
+    msg.reply('there was an error trying to execute that command!');
   }
 });
