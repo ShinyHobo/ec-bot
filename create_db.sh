c29dee83700b4dea7dc049d199953dd8c509ecc1
ShinyHobo <shinyhobo1@gmail.com>
2023-05-17 18:01:39 -0500

    2023-05-17 update

--- create_db.sh
+++ create_db.sh
@@ -7,7 +7,7 @@ outdir="$2"
 # for chunked mode, we need to know the database size in bytes beforehand
 bytes="$(stat --printf="%s" "$indb")"
 # set chunk size to 10MiB (needs to be a multiple of the `pragma page_size`!)
-serverChunkSize=$((10 * 1024 * 1024))
+serverChunkSize=$((2 * 1024 * 4096))
 suffixLength=3
 rm -f "$outdir/db.sqlite3"*
 split "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
